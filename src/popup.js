674eb77b5765e683c05e1fd132ec6c4cdd88bb93
Dobberman <pangestubima89@gmail.com>
2024-02-17 09:52:18 +0700

    remove unused code

--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,13 @@
 document.querySelector("#gen").addEventListener("click", function () {
-    let slang = document.querySelector("#slang").value;
-    if (slang) {
-        navigator.clipboard.writeText(document.querySelector("#slang").value);
-    } else {
-        document.querySelector("#gen").innerHTML = "Loading...";
-        fetch("https://salin-abangku.vercel.app/api/one")
-            .then((res) => res.json())
-            .then((data) => {
-                document.querySelector("#slang").value = data;
-                document.querySelector("#gen").innerHTML = "Regenerate";
-                document.querySelector("#copy").classList.add("hover:opacity-100");
-                document.querySelector("#copy").classList.add("duration-300");
-            });
-    }
+    document.querySelector("#gen").innerHTML = "Loading...";
+    fetch("https://salin-abangku.vercel.app/api/one")
+        .then((res) => res.json())
+        .then((data) => {
+            document.querySelector("#slang").value = data;
+            document.querySelector("#gen").innerHTML = "Regenerate";
+            document.querySelector("#copy").classList.add("hover:opacity-100");
+            document.querySelector("#copy").classList.add("duration-300");
+        });
 });
 
 document.querySelector("#copy").addEventListener("click", function () {
