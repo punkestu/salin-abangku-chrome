1f25f2661ba61b5e8f861fec7f0da198fc9dcd41
Dobberman <pangestubima89@gmail.com>
2024-02-17 09:50:40 +0700

    better copy mechanisme

--- src/popup.js
+++ src/popup.js
@@ -8,7 +8,13 @@ document.querySelector("#gen").addEventListener("click", function () {
             .then((res) => res.json())
             .then((data) => {
                 document.querySelector("#slang").value = data;
-                document.querySelector("#gen").innerHTML = "Copy";
+                document.querySelector("#gen").innerHTML = "Regenerate";
+                document.querySelector("#copy").classList.add("hover:opacity-100");
+                document.querySelector("#copy").classList.add("duration-300");
             });
     }
+});
+
+document.querySelector("#copy").addEventListener("click", function () {
+    navigator.clipboard.writeText(document.querySelector("#slang").value);
 });
